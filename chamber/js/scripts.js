c43b8a69c0ef28d1e1a9d920caa432c933c95113
Ezequiel Iannone <c.ezeiannone@gmail.com>
2022-05-14 17:58:51 -0300

    Preparing main and footer

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -24,6 +24,7 @@ const monthNames = [
   "December"
 ];
 
+// creates a Date object and gets the current year.
 const currentDate = new Date();
 const dayName = dayNames[currentDate.getDay()];
 const monthName = monthNames[currentDate.getMonth()];
@@ -31,13 +32,6 @@ const year = currentDate.getFullYear();
 const numberDay = currentDate.getDate();
 
 document.getElementById("current-date").textContent = `${dayName}, ${numberDay} ${monthName}, ${year}`;
-/*currentDateSpan = `${dayName}, ${monthName}, ${year}`;*/
-
-console.log();
-
-
-
-
 
 
 function toggleMenu() {
@@ -47,3 +41,14 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburger-btn");
 x.onclick = toggleMenu;
+
+
+// gets the first span tag and sets the year.
+document.querySelector("#year").textContent = year;
+
+// gets the second span tag and stored it.
+let lastUpdate = document.querySelector("#updated-date");
+// gets and stores a string containing the date and time on which the current document was last modified.
+let lastChangeDate = document.lastModified;
+// puts the previous information in the second span.
+lastUpdate.textContent = `Last Modification: ${lastChangeDate}`;
